35a3a8a3078f7cd0973d9f41f6272a2b73a9e821
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2020-09-01 14:27:50 -0700

    WIP

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -4,6 +4,22 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		
+		Mammals panda = new Mammals("Panda", 1869);
+		Mammals zebra = new Mammals("Zebra", 1778);
+		Mammals koala = new Mammals("Koala", 1816);
+		Mammals sloth = new Mammals("Sloth", 1804);
+		Mammals armadillo = new Mammals("Armadillo", 1758);
+		Mammals raccoon = new Mammals("Raccoon", 1758);
+		Mammals bigfoot = new Mammals("Bigfoot", 2021);
+
+		Birds pigeon = new Birds("Pigeon", 1837);
+		Birds peacock = new Birds("Peacock", 1821);
+		Birds toucan = new Birds("Toucan", 1758);
+		Birds parrot = new Birds("Parrot", 1824);
+		Birds swan = new Birds("Swan", 1758);
+
+		Fish salmon = new Fish("Salmon", 1758);
+		Fish catfish = new Fish("Catfish", 1817);
+		Fish perch = new Fish("Perch", 1758);
 	}
 }
